b62cb8be7b8079af0e880aaa48b26eca78aabf43
Johanna Räihälä <johanna.raihala@gmail.com>
2021-03-09 12:35:50 +0100

    Add configuration

--- util/database.js
+++ util/database.js
@@ -1,14 +1,17 @@
 import postgres from 'postgres';
+import { setPostgresDefaultsOnHeroku } from '../util/setPostgresDefaultsOnHeroku';
 require('dotenv-safe').config();
 const camelcaseKeys = require('camelcase-keys');
-
-// const sql = postgres();
 
 function connectOneTimeToDatabase() {
   let sql;
 
   if (process.env.NODE_ENV === 'production') {
-    sql = postgres({ ssl: true });
+    sql = postgres();
+    // Heroku needs SSL connections but
+    // has an "unauthorized" certificate
+    // https://devcenter.heroku.com/changelog-items/852
+    sql = postgres({ ssl: { rejectUnauthorized: false } });
   } else {
     if (!globalThis.__postgresSqlClient) {
       globalThis.__postgresSqlClient = postgres();

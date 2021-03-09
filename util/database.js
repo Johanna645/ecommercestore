a4eeb58e8eaad39f34a5e2405598d5f04816a69c
Johanna Räihälä <johanna.raihala@gmail.com>
2021-03-09 15:30:27 +0100

    Remove require

--- util/database.js
+++ util/database.js
@@ -9,7 +9,6 @@ function connectOneTimeToDatabase() {
   let sql;
 
   if (process.env.NODE_ENV === 'production') {
-    // sql = postgres();
     // Heroku needs SSL connections but
     // has an "unauthorized" certificate
     // https://devcenter.heroku.com/changelog-items/852
